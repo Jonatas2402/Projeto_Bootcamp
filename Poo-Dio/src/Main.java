f6f9d34934ef91cb2c787b7d34410db65e878c0c
jonatas <jonatas_dev@hotmail.com>
2025-10-04 17:34:30 -0300

    Initial commit

---
+++ Poo-Dio/src/Main.java
@@ -0,0 +1,27 @@
+import dominio.Curso;
+import dominio.Mentoria;
+
+import java.time.LocalDate;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(10);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição do curso JavaScript");
+        curso2.setCargaHoraria(20);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição da mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+    }
+}
